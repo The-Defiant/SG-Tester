8d4ecafc888faf07e1557a07afa39b9c78d4bbdc
PROJECT-DEFIANT <szymon.szyszkowski@mnm.bio>
2023-07-12 17:52:09 +0200

    [SGW-14] Basic CLI setup

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,52 @@
-use clap::Parser;
+use std::io::{stdout, Write};
+use clap::{Parser, Subcommand};
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
+#[command(version, about, long_about = None)]
+struct StructuredGeneCLI {
+    #[command(subcommand)]
+    command: Option<StructuredGeneCLICommands>,
+}
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+#[derive(Subcommand, Debug)]
+enum StructuredGeneCLICommands {
+    /// Create an API to fetch vcf content
+    Watch {},
+    /// Generate a single vcf content
+    Generate {},
+}
+
+
+fn main() -> Result<(), ()> {
+    // https://stackoverflow.com/questions/50775023/why-do-i-get-an-error-when-pattern-matching-a-struct-like-enum-variant-with-fiel
+    let args: StructuredGeneCLI = parse_arguments();
+    let mode: &str = extract_command(&args);
+    let out = stdout();
+    let _c = writeln!(&out, "Using mode: {}", mode); 
+    Ok(())
+}
+
+
+fn parse_arguments() -> StructuredGeneCLI {
+    StructuredGeneCLI::parse()}
+
+fn extract_command(args:  &StructuredGeneCLI) -> &str {
+    match args.command.as_ref().unwrap() {
+        StructuredGeneCLICommands::Watch {} => "watch",
+        StructuredGeneCLICommands::Generate {} => "generate",
+    }
+}
+
+
+#[test]
+fn test_command_extraction() {
+    let mut test_args = StructuredGeneCLI { command : None, };
+    test_args.command = Some(StructuredGeneCLICommands::Watch {});
+    assert_eq!(extract_command(&test_args), "watch");
+    test_args.command = Some(StructuredGeneCLICommands::Generate{});
+    assert_eq!(extract_command(&test_args), "generate");
 }
 
 
 
-fn main() {
-    println!("SGN is running")
 
-}
