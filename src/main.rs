42b48e654cd89d86354d345d8bf0daad1fb3727f
PROJECT-DEFIANT <szymon.szyszkowski@mnm.bio>
2023-07-14 22:19:06 +0200

    [SGW-14] Method to get formatfield for vcf file

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ fn main() -> Result<(), ()> {
     // https://stackoverflow.com/questions/50775023/why-do-i-get-an-error-when-pattern-matching-a-struct-like-enum-variant-with-fiel
     let args: StructuredGeneCLI = StructuredGeneCLI::parse();
     let mode: &str = extract_command(&args);
+    let _file_format = VcfParser::get_file_format();
     info!("We are running in {}", mode);
     Ok(())
 }
@@ -32,12 +33,28 @@ fn extract_command(args: &StructuredGeneCLI) -> &str {
     }
 }
 
+#[derive(Debug)]
+struct VcfParser {
+}
+
+impl VcfParser {
+    fn get_file_format() -> &'static[u8] {
+        b"##fileformat=VCFv4.2"
+    }
+}
+
 #[test]
 fn test_command_extraction() {
-    let mut test_args = StructuredGeneCLI { command : StructuredGeneCLICommands::Watch {} };
+    let mut test_args = StructuredGeneCLI {
+        command: StructuredGeneCLICommands::Watch {},
+    };
     assert_eq!(extract_command(&test_args), "watch");
     test_args.command = StructuredGeneCLICommands::Generate {};
     assert_eq!(extract_command(&test_args), "generate");
 }
-
+#[test]
+fn header_is_created() {
+    let expected_header = b"##fileformat=VCFv4.2";
+    assert_eq!(VcfParser::get_file_format(), expected_header);
+}
 
