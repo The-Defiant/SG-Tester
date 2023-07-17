ec8deb5813f3be02d9a0ea0d18a7cade4618f71d
PROJECT-DEFIANT <szymon.szyszkowski@mnm.bio>
2023-07-17 22:29:45 +0200

    [SGW-14] Attempt to write an INFO field

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,14 @@ struct StructuredGeneCLI {
     command: StructuredGeneCLICommands,
 }
 const ENSEMBL_CHROMOSOMES: [&str; 24] = [
-    "1", "2", "3", "4", "5", "6", "7", "8", "9", 
-    "10", "11", "12", "13", "14", "15", "16", "17", 
-    "18", "19", "20", "21", "X", "Y", "M"
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17",
+    "18", "19", "20", "21", "X", "Y", "M",
 ];
 const UCSC_CHROMOSOMES: [&str; 24] = [
-    "chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", 
-    "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", 
-    "chr18", "chr19", "chr20", "chr21", "chrX", "chrY", "chrM"
+    "chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11",
+    "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21",
+    "chrX", "chrY", "chrM",
 ];
-
 
 #[derive(Subcommand, Debug)]
 enum StructuredGeneCLICommands {
@@ -39,14 +37,14 @@ enum StructuredGeneCLICommands {
         #[arg(short, long, default_value = "true")]
         only_header: bool,
         #[arg(long, default_value = "ensembl")]
-        chrom_set: Option<ChromSet>
+        chrom_set: Option<ChromSet>,
     },
 }
 
 #[derive(Clone, Debug, ValueEnum)]
 enum ChromSet {
     Ensembl,
-    Ucsc
+    Ucsc,
 }
 
 fn main() -> Result<(), ()> {
@@ -75,18 +73,20 @@ fn command_dispatcher(args: &StructuredGeneCLI) -> Result<(), ()> {
             // create a server
             Ok(())
         }
-        StructuredGeneCLICommands::Generate { file, chrom_set, .. } => {
+        StructuredGeneCLICommands::Generate {
+            file, chrom_set, ..
+        } => {
             // write to a file
             let chromosomes = match chrom_set {
                 Some(ChromSet::Ensembl) => ENSEMBL_CHROMOSOMES,
                 Some(ChromSet::Ucsc) => UCSC_CHROMOSOMES,
-                None => ENSEMBL_CHROMOSOMES
+                None => ENSEMBL_CHROMOSOMES,
             };
             info!("Using chrom set from {:?}.", chrom_set.clone().unwrap());
             info!("Using chromosomes: {:?}.", chromosomes);
             let new_file = File::create(file).unwrap();
-
-
+            let s = String::from("This is length of the variant");
+            let _new_description = InfoField::parse_description(s);
             WritePlanner::new()
                 .add(VcfParser::get_file_format())
                 .add(b"some path")
@@ -95,6 +95,43 @@ fn command_dispatcher(args: &StructuredGeneCLI) -> Result<(), ()> {
         }
     }
 }
+
+
+// enum InfoFieldType {
+//     Integer,
+//     Float,
+//     Flag,
+//     Character,
+//     String
+// }
+
+// enum InfoNumberValue {
+//     PerAllele,    // R
+//     PerAltAllele, // A
+//     PerGenotype,  // G
+//     Known,        //i32
+//     Unknown,       // .
+//     FlagEntry     // 0
+// }
+
+
+struct InfoField {
+    // number: InfoNumberValue,  // Integer or value that describes the number of values that INFO can hold
+    // r#type: InfoFieldType, //
+    // description: String,
+    // source: String,
+    // version: String
+}
+
+
+impl InfoField {
+    fn parse_description(description: String) -> String {
+        let quoted_description = format!("\"{}\"", description);
+        quoted_description
+    }
+
+}
+
 
 struct VcfParser {}
 
@@ -135,7 +172,6 @@ impl<'a> WritePlanner<'a> {
     }
 }
 
-
 #[test]
 fn test_command_extraction() {
     let mut test_args = StructuredGeneCLI {
@@ -146,7 +182,7 @@ fn test_command_extraction() {
         file: PathBuf::from("/tmp/path"),
         chrom_set: None,
         number_of_variants: Some(1),
-        only_header: false
+        only_header: false,
     };
     assert_eq!(extract_command(&test_args), "generate");
 }
@@ -184,3 +220,9 @@ fn write_planner_writes_to_file() {
 }
 
 
+#[test]
+fn string_is_quoted() {
+    let s: String = String::from("Dummy");
+    let result = InfoField::parse_description(s);
+    assert_eq!(String::from("\"Dummy\""), result)
+}
