5ed5ce043ecec36292a9b7ab31fb50e79c68a12a
PROJECT-DEFIANT <szymon.szyszkowski@mnm.bio>
2023-07-20 19:15:55 +0200

    [SGW-14] Moved chromosome constants to a separate module

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,15 @@ use log::info;
 use std::env;
 use std::fs::File;
 use std::path::PathBuf;
-
+mod boundaries;
 mod write_planner;
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct StructuredGeneCLI {
     #[command(subcommand)]
     command: StructuredGeneCLICommands,
 }
-const ENSEMBL_CHROMOSOMES: [&str; 24] = [
-    "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17",
-    "18", "19", "20", "21", "X", "Y", "M",
-];
-const UCSC_CHROMOSOMES: [&str; 24] = [
-    "chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11",
-    "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21",
-    "chrX", "chrY", "chrM",
-];
-
 #[derive(Subcommand, Debug)]
 enum StructuredGeneCLICommands {
     /// Create an API to fetch vcf content
@@ -77,9 +68,9 @@ fn command_dispatcher(args: &StructuredGeneCLI) -> Result<(), ()> {
         } => {
             // write to a file
             let chromosomes = match chrom_set {
-                Some(ChromSet::Ensembl) => ENSEMBL_CHROMOSOMES,
-                Some(ChromSet::Ucsc) => UCSC_CHROMOSOMES,
-                None => ENSEMBL_CHROMOSOMES,
+                Some(ChromSet::Ensembl) => boundaries::ENSEMBL_CHROMOSOMES,
+                Some(ChromSet::Ucsc) => boundaries::UCSC_CHROMOSOMES,
+                None => boundaries::ENSEMBL_CHROMOSOMES,
             };
             info!("Using chrom set from {:?}.", chrom_set.clone().unwrap());
             info!("Using chromosomes: {:?}.", chromosomes);
